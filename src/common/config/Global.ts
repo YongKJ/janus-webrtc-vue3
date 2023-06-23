6a23967d4b3c7ad462c7026031ad25f3c8b09930
dengxiaojun <dxj1718874198@gmail.com>
2023-06-23 20:40:18 +0800

    [update]janus stream test page

--- src/common/config/Global.ts
+++ src/common/config/Global.ts
@@ -2,10 +2,8 @@
 export class Global {
 
     private static readonly _LOG_ENABLE: boolean = true;
-    private static readonly _WEBRTC_PEER_BASE_URL = "https://esc.yongkj.cn";
-    private static readonly _JANUS_GATEWAY_BASE_URL = "wss://janus.yongkj.cn";
-    // private static readonly _JANUS_GATEWAY_BASE_URL = "wss://janus.conf.meetecho.com/ws";
-    // private static readonly _JANUS_GATEWAY_BASE_URL = "wss://audio.whaleshub.cn:8989";
+    private static readonly _WEBRTC_PEER_BASE_URL = "";
+    private static readonly _JANUS_GATEWAY_BASE_URL = "";
 
     static get LOG_ENABLE(): boolean {
         return this._LOG_ENABLE;
